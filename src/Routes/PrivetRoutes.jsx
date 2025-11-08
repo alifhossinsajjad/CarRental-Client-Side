0086a75c3cc3bf85c23c7dcffb40e914f4091605
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-11-08 23:33:53 +0600

    create a another route for privet routes

--- src/Routes/PrivetRoutes.jsx
+++ src/Routes/PrivetRoutes.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { use } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import { Navigate, useLocation } from "react-router";
 
-const PrivetRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivetRoutes = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (user && user?.email) {
+    return children;
+  }
+  return (
+    <div>
+      <Navigate state={location.pathname} to={"/auth/login"} replace />
+    </div>
+  );
 };
 
 export default PrivetRoutes;
