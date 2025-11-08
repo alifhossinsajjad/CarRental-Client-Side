86583201a13a3ea3a261bd0046c7596fc4963924
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-11-08 23:17:52 +0600

    context setup done

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { auth } from '../Firebase/Firebase.config';
+import { AuthContext } from './AuthContext';
 
-const AuthProvider = () => {
+
+const AuthProvider = ({children}) => {
+
+    const [user,setUser] = useState(null)
+
+    const [loading, setLoading] = useState(true)
+
+
+
+useEffect(()=> {
+    const unsubscribe = onAuthStateChanged(auth,(currentUser) =>{
+        setUser(currentUser)
+        setLoading(false)
+    })
+    return()=> {
+        unsubscribe()
+    } 
+},[])
+
+const userInfo ={
+    user,
+    setUser,
+    loading,
+    setLoading,
+} 
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
     );
 };
 
