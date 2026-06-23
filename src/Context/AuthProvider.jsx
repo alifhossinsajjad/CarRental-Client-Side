663d2caeb395f30b2c785dc7241e5bcc054935d5
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-06-23 15:50:49 +0600

    fix the login issues

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -23,6 +23,9 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+
+  
+
   const signInUser = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
